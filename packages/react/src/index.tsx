05026e97a88d06ed4d7e17145674d67f5663e9d1
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2023-03-23 21:54:55 -0300

    Story: Button

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,12 +1,32 @@
+import { ComponentProps } from 'react'
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$default',
-  backgroundColor: '$ignite500',
+  backgroundColor: '$ignite300',
   borderRadius: '$md',
-  padding: '$4',
+
+  border: 0,
+  fontWeights: 'bold',
+  colors: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
 })
 
-export function App() {
-  return <Button>Hello World</Button>
-}
+export type ButtonProps = ComponentProps<typeof Button>
