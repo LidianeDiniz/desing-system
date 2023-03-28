f1610ae3f10f6b9509c9fc4eb71c4b72be3984fa
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2023-03-27 22:54:02 -0300

    Componente: Button

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,50 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Button, ButtonProps } from '@ignite-ui/react'
+import { ArrowArcRight } from 'phosphor-react'
+
+export default {
+  title: 'Form/Button',
+  component: Button,
+  args: {
+    children: 'Send',
+  },
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'secondary',
+    children: 'Create new',
+  },
+}
+
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'tertiary',
+    children: 'Cancel',
+  },
+}
+
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm',
+  },
+}
+
+export const WithIcon: StoryObj<ButtonProps> = {
+  args: {
+    children: (
+      <>
+        Próximo passo
+        <ArrowArcRight weight="bold" />
+      </>
+    ),
+  },
+}
+
+export const Disable: StoryObj<ButtonProps> = {
+  args: {
+    disabled: true,
+  },
+}
