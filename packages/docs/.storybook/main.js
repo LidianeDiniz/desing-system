2dc1fc124cec63fd231df58921f2af4ab1a5a133
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2023-03-28 22:33:08 -0300

    fix: Add aubpath to Storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -15,5 +15,13 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
-  }
-}
+  },
+
+  viteFinal: (config, { configType }) => {
+    if (configType === "PRODUCTION") {
+      config.base = "/desing-system/";
+    }
+
+    return config;
+  },
+};
